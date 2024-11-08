ec89aa88a3d353f25ed727248857586539d35ebe
Matei Teca <matei.teca@gmail.com>
2024-11-08 11:48:28 +0200

    footer nav

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,29 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/Footer.css";
 
 const Footer = () => (
-
-
-<footer className="footer">
-<p>&copy; {new Date().getFullYear()} Dental Implants Clinic</p>
-<p>Contact us at: (555) 555-5555</p>
-</footer>
-
+  <footer className="footer">
+    <div className="footer-nav">
+      <ul>
+        <li>
+          <Link to="/">
+            <img 
+              src="https://static.thenounproject.com/png/610387-200.png" 
+              alt="Home Icon" 
+              className="footer-home-icon"
+            />
+          </Link>
+        </li>
+        <li><Link to="/about">About</Link></li>
+        <li>Services</li>
+        <li>Testimonials</li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </div>
+    <p>&copy; {new Date().getFullYear()} O.C. Dental Implants</p>
+    <p>Contact us at: (555) 555-5555</p>
+  </footer>
 );
 
 export default Footer;
