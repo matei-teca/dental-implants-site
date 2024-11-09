c2f4cc37334b7466e44b7f8c47493468922e37c5
Matei Teca <matei.teca@gmail.com>
2024-11-09 10:08:28 +0200

    book an appointment wwith google calendar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,32 +3,49 @@ import { Link } from "react-router-dom";
 import "./styles/Header.css";
 
 const Header = () => (
+  <header className="header">
+    <div className="logo">
+      <Link to="/">
+        <img
+          src="https://static.thenounproject.com/png/610387-200.png"
+          alt="Home Icon"
+          style={{ width: "20px", marginRight: "10px" }}
+        />
+        O.C. Dental Implants
+      </Link>
+    </div>
 
-<header className="header">
-<div className="logo">
-<Link to="/">
-  <img 
-    src="https://static.thenounproject.com/png/610387-200.png" 
-    alt="Home Icon" 
-    style = {{width: "20px", marginRight: "10px"}}
-  /> 
-  O.C. Dental Implants
-</Link></div>
-
-<nav>
-  <ul>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/services">Services</Link></li>
-    <li><Link to="/testimonials">Testimonials</Link></li>
-    <li><Link to="/showcases">ShowCases</Link></li>
-    <li><Link to="/contact">Contact</Link></li>
-    <li>
-      <button className="cta-button"><Link to="/contact">Book an Appointment</Link></button>
-    </li>
-  </ul>
-</nav>
-</header>
-
+    <nav>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/testimonials">Testimonials</Link>
+        </li>
+        <li>
+          <Link to="/showcases">ShowCases</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <button className="cta-button">
+            <a
+              href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Appointment+with+O.C.+Dental+Implants&details=Please+confirm+the+appointment+with+our+office.&location=O.C.+Dental+Implants+Office&sf=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Book an Appointment
+            </a>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;
