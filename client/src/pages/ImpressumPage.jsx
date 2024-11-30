bc59c4d449659a2747be76eef9183a70bc6c71b6
Matei Teca <matei.teca@gmail.com>
2024-11-30 21:08:41 +0200

    language impressumPage

--- client/src/pages/ImpressumPage.jsx
+++ client/src/pages/ImpressumPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useLanguage } from '../LanguageContext';
 import './styles/ImpressumPage.css';
 
+const texts = {
+    en: ["Contact Details", "Name", "Address", "Email", "Phone", "Professional Registration:", "VAT ID:"],
+    de: ["Kontaktdaten", "Name", "Adresse", "E-Mail", "Telefon", "Berufliche Registrierung:", "USt-IdNr.:"]
+  }
+
 const ImpressumPage = () => {
+    const { language } = useLanguage();
+
     return (
         <div>
             <header className="impressum-header">
@@ -9,13 +17,13 @@ const ImpressumPage = () => {
                 <p>Legal Notice and Contact Information</p>
             </header>
             <div className="impressum-info">
-                <h2>Contact Details</h2>
-                <p><strong>Name:</strong> [Your Full Name]</p>
-                <p><strong>Address:</strong> [Your Address]</p>
-                <p><strong>Email:</strong> [Your Email]</p>
-                <p><strong>Phone:</strong> [Your Phone Number]</p>
-                <p><strong>Professional Registration:</strong> [Zahnärztekammer registration number]</p>
-                <p><strong>VAT ID:</strong> [Your VAT ID, if applicable]</p>
+                <h2>{texts[language][0]}</h2>
+                <p><strong>{texts[language][1]}</strong> [Your Full Name]</p>
+                <p><strong>{texts[language][2]}</strong> [Your Address]</p>
+                <p><strong>{texts[language][3]}</strong> [Your Email]</p>
+                <p><strong>{texts[language][4]}</strong> [Your Phone Number]</p>
+                <p><strong>{texts[language][5]}</strong> [Zahnärztekammer registration number]</p>
+                <p><strong>{texts[language][6]}</strong> [Your VAT ID, if applicable]</p>
             </div>
         </div>
     );
